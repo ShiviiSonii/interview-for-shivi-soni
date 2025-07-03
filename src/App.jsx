1ef22cbb11bea9fbef28ab1d1a3a32117fb36104
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-07-03 15:29:56 +0530

    Add Shadcn UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [date, setDate] = useState(new Date())
+
   return (
-    <h1 class="text-3xl font-bold underline">
+    <>
+    <h1 class="text-2xl flex justify-center items-center h-screen font-bold">
       Welcome to SpaceX!
     </h1>
+    </>
   )
 }
 
