7abf8d4d09519d5cab34197621aeda2ec2b5bf7a
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-07-03 20:45:12 +0530

    Add Header component with SpaceX logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex items-center justify-center border-b pb-4 mb-4">
+      <img src="/logo.png" alt="spacex logo" width={160} height={"auto"}/>
+    </div>
+  );
+}
